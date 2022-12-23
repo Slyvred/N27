3df792540b3aed5fdee8f0388af79617c5399c4f
Slyvred <remihage2003@gmail.com>
2022-12-23 18:37:39 +0100

    j'avais oublié d'inclure les .hpp

---
+++ classes/agency.hpp
@@ -0,0 +1,61 @@
+//
+// Created by remi on 30/11/22.
+//
+#pragma once
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <chrono>
+#include <fstream>
+#include <string>
+#include "json.hpp"
+#include "user.hpp"
+
+using namespace std;
+using json = nlohmann::json;
+
+#ifndef N27_AGENCY_H
+#define N27_AGENCY_H
+
+typedef struct Transaction {
+    int from_acc, to_acc;
+    float amount;
+    long timestamp;
+}transaction;
+
+
+class agency {
+private:
+    unordered_map<int, user> users;
+    unordered_map<int, account> accounts;
+    vector<transaction> transactions;
+    int id;
+    int n_users;
+public:
+
+    agency();
+
+    void createUser(infos& infos, int n_accounts);
+    void deleteUser(int id);
+    user& getUser(int id);
+    void send(int from_acc, int to_acc, float amount);
+    void deposit(int to_acc, float amount);
+
+    json exportUsers() const;
+    json exportAccounts() const;
+    json exportTransactions() const;
+    int getId() const;
+
+    void importUsers();
+    void importAcounts();
+    void importTransactions();
+
+    const unordered_map<int, user>& getUsers() const;
+    const unordered_map<int, account>& getAccounts() const;
+    const vector<transaction>& getTransactions() const;
+
+    void update();
+};
+
+
+#endif //N27_AGENCY_H
---
+++ classes/user.hpp
@@ -0,0 +1,39 @@
+//
+// Created by remi on 30/11/22.
+//
+#pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include "account.hpp"
+
+using namespace std;
+
+#ifndef N27_USER_H
+#define N27_USER_H
+
+typedef struct Infos {
+    string nom, prenom, addr;
+}infos;
+
+
+class user {
+private:
+    infos m_infos;
+    int n_accounts;
+    int id;
+    vector<int> accounts;
+public:
+    user(infos& infos, int n_accounts);
+    int getId() const;
+    void setId(int id);
+    const infos& getInfos() const;
+    const vector<int>& getAccounts() const;
+    const int& getAccount(int index) const;
+    void addAccount(int id);
+    friend ostream& operator<<(ostream& os, const user& user);
+};
+
+
+#endif //N27_USER_H
